e58633a5f0fe69f2cc9860ff17a3b3997b743542
Le Anh Duc <ducvui2003@gmail.com>
2025-10-25 17:29:43 +0700

    add loading and blur in category and sub category

---
+++ client/src/app/[category]/[sub-category]/[product]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-40 items-center justify-center">
+      <p className="animate-pulse text-gray-500">Loading...</p>
+    </div>
+  );
+}
